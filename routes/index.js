ce2db74346fe2b9ac993dd7d1d03f89f2bb83986
Christian Mueller <christian.mueller@kii.com>
2014-05-20 15:39:20 +0900

    add topic info

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ exports.index = function(req, res){
             res.send('Ok');
         });
     } else if (sns.Type == 'Notification') {
-        var message = '';
+        var message = sns.TopicArn.substring(sns.TopicArn.lastIndexOf(':')) + ' : ';
         if (sns.Subject === undefined) {
             message = JSON.stringify(sns.Message);
         } else {
